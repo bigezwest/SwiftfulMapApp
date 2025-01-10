78770f73f4222223afb61c62b6ac86b2a62a201d
Thomas <88990664+bigezwest@users.noreply.github.com>
2025-01-10 21:32:31 +0100

    [Feature] Add LocationPreviewView to LocationView - This updates the preview to the selected list item

--- SwiftfulMapApp/Views/LocationsView.swift
+++ SwiftfulMapApp/Views/LocationsView.swift
@@ -21,6 +21,15 @@ struct LocationsView: View {
                 header
                     .padding()
                 Spacer()
+                ZStack {
+                    ForEach(vm.locations) { location in
+                        if vm.mapLocation == location {
+                            LocationPreviewView(location: location)
+                                .shadow(color: Color.black.opacity(0.3), radius: 20)
+                                .padding()
+                        }
+                    }
+                }
             }
         }
     }
