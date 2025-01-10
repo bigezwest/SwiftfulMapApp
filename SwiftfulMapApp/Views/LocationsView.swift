cc0b67e37eabd7ee6bfbb417619efc4f35471e46
Thomas <88990664+bigezwest@users.noreply.github.com>
2025-01-10 21:37:47 +0100

    [Feature] Add transition when chaning selected list item

--- SwiftfulMapApp/Views/LocationsView.swift
+++ SwiftfulMapApp/Views/LocationsView.swift
@@ -27,6 +27,7 @@ struct LocationsView: View {
                             LocationPreviewView(location: location)
                                 .shadow(color: Color.black.opacity(0.3), radius: 20)
                                 .padding()
+                                .transition(.asymmetric(insertion: .move(edge: .trailing), removal: .move(edge: .leading)))
                         }
                     }
                 }
