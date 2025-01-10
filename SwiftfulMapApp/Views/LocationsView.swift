74190495a1eb1a3206e2f44e099bca5238c7d9eb
Thomas <88990664+bigezwest@users.noreply.github.com>
2025-01-10 11:57:20 +0100

    [Feature] Create LocationsView

---
+++ SwiftfulMapApp/Views/LocationsView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationsView.swift
+//  SwiftfulMapApp
+//
+//  Created by Thomas on 1/10/25.
+//
+
+import SwiftUI
+
+struct LocationsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationsView()
+}
