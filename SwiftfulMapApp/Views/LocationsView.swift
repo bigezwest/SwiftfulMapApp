43e232edb44d30e2ecec9dd8a54b6f43be784e5e
Thomas <88990664+bigezwest@users.noreply.github.com>
2025-01-10 19:46:35 +0100

    [Feature] Move menu into LocationsView extention "header"

--- SwiftfulMapApp/Views/LocationsView.swift
+++ SwiftfulMapApp/Views/LocationsView.swift
@@ -18,18 +18,8 @@ struct LocationsView: View {
             Map(coordinateRegion: $vm.mapRegion)
                 .ignoresSafeArea()
             VStack (spacing: 0) {
-                VStack {
-                    Text(vm.mapLocation.name + ", " + vm.mapLocation.cityName)
-                        .font(.title2)
-                        .fontWeight(.black)
-                        .foregroundColor(.primary)
-                        .frame(height: 55)
-                        .frame(maxWidth: .infinity)
-                }
-                .background(.thickMaterial)
-                .cornerRadius(10)
-                .shadow(color: Color.black.opacity(0.3), radius: 20, x: 0, y: 15)
-                .padding()
+                header
+                    .padding()
                 Spacer()
             }
         }
@@ -39,4 +29,20 @@ struct LocationsView: View {
 #Preview {
     LocationsView()
         .environmentObject(LocationsViewModel())
+}
+
+extension LocationsView {
+    private var header: some View {
+        VStack {
+            Text(vm.mapLocation.name + ", " + vm.mapLocation.cityName)
+                .font(.title2)
+                .fontWeight(.black)
+                .foregroundColor(.primary)
+                .frame(height: 55)
+                .frame(maxWidth: .infinity)
+        }
+        .background(.thickMaterial)
+        .cornerRadius(10)
+        .shadow(color: Color.black.opacity(0.3), radius: 20, x: 0, y: 15)
+    }
 }
