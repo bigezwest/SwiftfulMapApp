6981865916a3fe08b3ad3293635b018441dc3abb
Thomas <88990664+bigezwest@users.noreply.github.com>
2025-01-10 23:49:04 +0100

    [Feature] Add LocationDetailView

---
+++ SwiftfulMapApp/Views/LocationDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationDetailView.swift
+//  SwiftfulMapApp
+//
+//  Created by Thomas on 1/10/25.
+//
+
+import SwiftUI
+
+struct LocationDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationDetailView()
+}
