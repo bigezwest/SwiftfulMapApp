046be2f78a8aed9c7aa2a5d55f7aad6be970d443
Thomas <88990664+bigezwest@users.noreply.github.com>
2025-01-10 15:52:13 +0100

    [Feature] Add map to LocationsView - Add State cameraPosition - Add ZStack to View

--- SwiftfulMapApp/Views/LocationsView.swift
+++ SwiftfulMapApp/Views/LocationsView.swift
@@ -11,12 +11,14 @@ import MapKit
 struct LocationsView: View {
 
     @EnvironmentObject private var vm: LocationsViewModel
+    @State private var cameraPosition = MapCameraPosition.region(MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 41.8902, longitude: 12.4922), span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
+    )
     
     var body: some View {
-        List {
-            ForEach(vm.locations) {
-                Text($0.name)
-            }
+        
+        ZStack {
+            Map(position: $cameraPosition)
+                .ignoresSafeArea()
         }
     }
 }
