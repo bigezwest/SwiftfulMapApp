722eb78672c938121d232cc7c3fc463bb5be6ba3
Thomas <88990664+bigezwest@users.noreply.github.com>
2025-01-10 13:09:31 +0100

    [Feature] Create LocationsViewModel class and vm StateObject

--- SwiftfulMapApp/Views/LocationsView.swift
+++ SwiftfulMapApp/Views/LocationsView.swift
@@ -7,7 +7,14 @@
 
 import SwiftUI
 
+class LocationsViewModel: ObservableObject {
+    
+}
+
 struct LocationsView: View {
+    
+    @StateObject private var vm = LocationsViewModel()
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
