eca790396872d49d9fd2a37ed661047b56630d68
Thomas <88990664+bigezwest@users.noreply.github.com>
2025-01-10 19:43:10 +0100

    [Feature] Create menu

--- SwiftfulMapApp/Views/LocationsView.swift
+++ SwiftfulMapApp/Views/LocationsView.swift
@@ -17,6 +17,21 @@ struct LocationsView: View {
         ZStack {
             Map(coordinateRegion: $vm.mapRegion)
                 .ignoresSafeArea()
+            VStack (spacing: 0) {
+                VStack {
+                    Text(vm.mapLocation.name + ", " + vm.mapLocation.cityName)
+                        .font(.title2)
+                        .fontWeight(.black)
+                        .foregroundColor(.primary)
+                        .frame(height: 55)
+                        .frame(maxWidth: .infinity)
+                }
+                .background(.thickMaterial)
+                .cornerRadius(10)
+                .shadow(color: Color.black.opacity(0.3), radius: 20, x: 0, y: 15)
+                .padding()
+                Spacer()
+            }
         }
     }
 }
