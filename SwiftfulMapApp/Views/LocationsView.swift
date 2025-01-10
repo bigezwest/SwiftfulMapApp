7cd73c0b875f0d9fefbf4f11e1a66197a2b76e9a
Thomas <88990664+bigezwest@users.noreply.github.com>
2025-01-10 13:29:02 +0100

    [Feature] Access data in LocationDastaService - Confirm Location Model, LocationsView, and LocationDataService are connected correctly by displaying in View

--- SwiftfulMapApp/Views/LocationsView.swift
+++ SwiftfulMapApp/Views/LocationsView.swift
@@ -21,7 +21,11 @@ struct LocationsView: View {
     @StateObject private var vm = LocationsViewModel()
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            ForEach(vm.locations) {
+                Text($0.name)
+            }
+        }
     }
 }
 
