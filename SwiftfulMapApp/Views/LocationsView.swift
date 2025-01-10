222f5d36ff0b9e1d8cff07da7dd2f46a04ff9d1a
Thomas <88990664+bigezwest@users.noreply.github.com>
2025-01-10 16:30:35 +0100

    [Feature] Update LocationsViewModel - Add mapLocation - Add mapRegion - Add updateMapregion
    Update LocationsView to access LocationsViewModel

--- SwiftfulMapApp/Views/LocationsView.swift
+++ SwiftfulMapApp/Views/LocationsView.swift
@@ -11,13 +11,11 @@ import MapKit
 struct LocationsView: View {
 
     @EnvironmentObject private var vm: LocationsViewModel
-    @State private var cameraPosition = MapCameraPosition.region(MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 41.8902, longitude: 12.4922), span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
-    )
     
     var body: some View {
         
         ZStack {
-            Map(position: $cameraPosition)
+            Map(coordinateRegion: $vm.mapRegion)
                 .ignoresSafeArea()
         }
     }
